685cecdc7a53c03028010ccd070b4027b8a0c24a
zallas <jose_martinez2308@hotmail.com>
2024-06-02 14:38:42 -0600

    Updates

---
+++ 1863aa9b.js
@@ -0,0 +1,1 @@
+import{r}from"./fa7406cd.js";const o=document.body;o.addEventListener("changed-theme",function(t){const{detail:e}=t,s={moon:"sl-theme-dark","brightness-high":"sl-theme-light"};this.setAttribute("class",s[e])}),r("my-icons",{resolver:t=>"./assets/".concat(t,".svg"),mutator:t=>t.setAttribute("fill","currentColor")});
